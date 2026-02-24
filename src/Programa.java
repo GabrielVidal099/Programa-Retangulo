e9caaeb83a02a059461ae09d89737bcaeb324814
GabrielVidal099 <vidalgabriel2019@gmail.com>
2026-02-24 12:08:31 -0300

    Pequenas Mudanças!

--- src/Programa.java
+++ src/Programa.java
@@ -3,8 +3,6 @@ import entities.Rectangle;
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Programa {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
